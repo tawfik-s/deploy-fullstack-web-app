9dec8c04402ee197852ff3cebb38eb502d201647
tawfeek shalash <t.shalash1@gmail.com>
2022-09-21 12:53:05 +0200

    fixing deployment bugs

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-2svdf2dh.us-east-1.elasticbeanstalk.com",
+  apiHost:
+    "http://udagram-api-dev.eba-2svdf2dh.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
